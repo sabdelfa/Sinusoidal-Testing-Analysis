9b2d4105d15e316738b2f6f8792c127eaa1f5978
chrystalight <140845359+chrystalight@users.noreply.github.com>
2023-08-11 18:33:05 -0400

    Completed :)

--- Main.py
+++ Main.py
@@ -41,55 +41,36 @@ def read_csv(csv_name):
                 pass
             else:
                 definer = list(row[1]) 
-                #at index 1 we expect to see a string of the form int_word (e.g. 1_Stretch), but we only care if it is int_"stretch"
-                #that is going to give us a character array "1","_","S","t","r","e","t","c","h"
-                #definer[0] will tell us which cycle we're in (1-20)
-                #definer[2] will be either S (we want this data) or P or R (we dont want this data)
                 if sinusoid_count<10:
-                    if definer[2] == "P" or definer[2] == "R":
-                        pass
-                    else:
-                        #now we've determined we are in a stretch phase 
-                        if int(definer[0])==sinusoid_count:
-                            temp_displacement.append(float(row[4]))
-                            temp_force.append(float(row[5]))
-                            #add the force and displacement to the temp arrays
-                        else:
-                            #we're on a new sinusoid
-                            e = find_e(temp_force, temp_displacement, sinusoid_count)
-                            e_array.append(e)
-                            print("youngs modulus for sinusoid", sinusoid_count, "is", e)
-                            sinusoid_count = sinusoid_count+1
-                            temp_displacement = []
-                            temp_force = []
-                            #so append the two arrays to our bigger array, empty them, and start brand new
-                            #and then increment sinusoid_count
+                    cycle = definer[2]
+                    curr_sin = int(definer[0])
                 else:
-                    if definer[3] == "P" or definer[3] == "R":
-                        pass
+                    cycle = definer[3]
+                    curr_sin = int(definer[0]+definer[1])
+                #cycle will define whether we're in Preload ("P"), Stretch ("S"), or Recovery ("R")
+
+                if cycle == "P" or cycle == "R":
+                    pass
+                else:
+                    #now we've determined we are in a stretch phase 
+                    if curr_sin==sinusoid_count:
+                        temp_displacement.append(float(row[4]))
+                        temp_force.append(float(row[5]))
+                        #add the force and displacement to the temp arrays
                     else:
-                        #now we've determined we are in a stretch phase 
-                        if int(definer[0]+definer[1])==sinusoid_count:
-                            temp_displacement.append(float(row[4]))
-                            temp_force.append(float(row[5]))
-                            #add the force and displacement to the temp arrays
-                        else:
-                            #we're on a new sinusoid
-                            e = find_e(temp_force, temp_displacement, sinusoid_count)
-                            e_array.append(e)
-                            print("youngs modulus for sinusoid", sinusoid_count, "is", e)
-                            sinusoid_count = sinusoid_count+1
-                            temp_displacement = []
-                            temp_force = []
-                            #so append the two arrays to our bigger array, empty them, and start brand new
-                            #and then increment sinusoid_count                    
+                        #we're on a new sinusoid
+                        e_array.append(find_e(temp_force, temp_displacement, sinusoid_count))
+                        sinusoid_count = sinusoid_count+1
+                        temp_displacement = []
+                        temp_force = []
+                        #so append the two arrays to our bigger array, empty them, and start brand new
+                        #and then increment sinusoid_count                
             line_count += 1
-        e = find_e(temp_force, temp_displacement, sinusoid_count)
-        e_array.append(e)
-        print("youngs modulus for sinusoid", sinusoid_count, "is", e)
-        sinusoid_count = sinusoid_count+1
+        #need this one more time to account for final sinusoid
+        e_array.append(find_e(temp_force, temp_displacement, sinusoid_count))
+
         print(f'Processed {line_count} lines.')
-        print(e_array)
+        e_array.insert(0,"Youngs Modulus for Sinuoids")
         pd.DataFrame(e_array).to_csv(active_name+"/"+active_name+'youngs modulus.csv')    
 
 def find_e(force, displacement, sinusoid_count):
@@ -99,7 +80,6 @@ def find_e(force, displacement, sinusoid_count):
     #force: an array of doubles represeting force in newtons
     #displacement: an array of doubles representing displacement in mm
     #------------------------------------
-    print("force array length is", len(force), "displacement array length is", len(displacement))
     stress_array = []
     strain_array = []
 
@@ -115,6 +95,8 @@ def find_e(force, displacement, sinusoid_count):
 
     stress_strain_plot(stress_array, strain_array, sinusoid_count)
     res = scipy.stats.linregress(stress_array, strain_array)
+
+    print("Youngs modulus for sinusoid cycle", sinusoid_count, "is", float(res[0]))
     return float(res[0])
 
 def stress_strain_plot(stress, strain, sinusoid_count):
